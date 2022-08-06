f29e0e77aa47961282dd545a612c561829fc4bb5
mika.urakawa <awakaru0122@gmail.com>
2022-08-06 11:24:50 +0900

    Initial Commit

---
+++ BuildBotTest/BuildBotTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildBotTestApp.swift
+//  BuildBotTest
+//
+//  Created by mikaurakawa on 2022/08/06.
+//
+
+import SwiftUI
+
+@main
+struct BuildBotTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
